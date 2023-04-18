5c09a55fd6ddc5bf022a3610e19bf8976cc4a90c
elianCoMa <elianhcb@gmail.com>
2023-04-17 19:12:13 -0500

    creacion

---
+++ webOnpe-1/src/main/java/com/onpe/WebOnpe1Application.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOnpe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOnpe1Application.class, args);
+	}
+
+}
